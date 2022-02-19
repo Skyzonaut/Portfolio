86dbbd10c33a35334fefa39ed1aaee3a016ca7f3
Yohan Folliot <yohan.folliot@asap-info.com>
2022-02-19 01:34:46 +0100

    Ajout des compétences et refonte de la frise chronologique

--- script.js
+++ script.js
@@ -13,3 +13,46 @@ function active() {
         graph.style.visibility = 'hidden';
     }
 }
+
+function clickTech() {
+
+    var techLbl = document.getElementById('tech-lbl');
+    var skillLbl = document.getElementById('skill-lbl');
+    var tech = document.getElementById('tech');
+    var skill = document.getElementById('skill');
+        skill.style.display = 'none'; //or
+        skill.style.visibility = 'hidden';
+
+        tech.style.display = 'flex'; //or
+        tech.style.visibility = 'visible';
+
+        techLbl.style.color = '#9191ec';
+        techLbl.style.fontWeight = 'bolder';
+
+        skillLbl.style.color = 'white';
+        skillLbl.style.fontWeight = 'normal';
+
+        console.log("fils de pute");
+}
+
+
+function clickSkill() {
+
+    var techLbl = document.getElementById('tech-lbl');
+    var skillLbl = document.getElementById('skill-lbl');
+    var tech = document.getElementById('tech');
+    var skill = document.getElementById('skill');
+
+        tech.style.display = 'none'; //or
+        tech.style.visibility = 'hidden';
+
+        skill.style.display = 'flex'; //or
+        skill.style.visibility = 'visible';
+
+        skillLbl.style.color = '#9191ec';
+        skillLbl.style.fontWeight = 'bolder';
+
+        techLbl.style.color = 'white';
+        techLbl.style.fontWeight = 'normal';
+
+}
